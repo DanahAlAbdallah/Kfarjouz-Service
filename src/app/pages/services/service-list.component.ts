701bf5b6838915702e6935b05efe4a67cb9807f2
DanahAlAbdallah <danaalabdallah02@gmail.com>
2026-01-08 21:27:52 +0200

    category filter + shimmer

--- src/app/pages/services/service-list.component.ts
+++ src/app/pages/services/service-list.component.ts
@@ -14,28 +14,46 @@ import { Service } from '../../models/service.model';
 })
 export class ServicesListComponent implements OnInit {
 
-  constructor(private servicesService: ServicesService) {}
+  constructor(private servicesService: ServicesService) { }
 
   // 🔍 search input state
   searchTerm = signal('');
+  selectedCategory = signal<'all' | 'Restaurant' | 'Teacher'>('all');
+  isLoading = signal(true);
 
-  ngOnInit() {
+ ngOnInit() {
+  this.isLoading.set(true);
+
+  // simulate async fetch (replace later with HttpClient)
+  setTimeout(() => {
     const allServices = this.servicesService.getAll();
     servicesSignal.set(allServices);
-  }
+    this.isLoading.set(false);
+  }, 800);
+}
 
-  // ✅ filtered services (reactive)
+
   filteredServices = computed(() => {
     const query = this.searchTerm().toLowerCase().trim();
+    const category = this.selectedCategory();
 
-    return servicesSignal().filter(service =>
-      service.title.toLowerCase().includes(query) ||
-      service.category.toLowerCase().includes(query) ||
-      (service.description ?? '').toLowerCase().includes(query)
-    );
+    return servicesSignal().filter(service => {
+      const matchesSearch =
+        service.title.toLowerCase().includes(query) ||
+        service.category.toLowerCase().includes(query) ||
+        (service.description ?? '').toLowerCase().includes(query);
+
+      const matchesCategory =
+        category === 'all' || service.category === category;
+
+      return matchesSearch && matchesCategory;
+    });
   });
 
   trackById(_: number, service: Service) {
     return service.id;
   }
+
+
+
 }
