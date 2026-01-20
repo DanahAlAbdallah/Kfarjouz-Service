21974e9cb3a86e378bbc78621e201842eb9decd6
DanahAlAbdallah <danaalabdallah02@gmail.com>
2026-01-20 23:14:03 +0200

    hero section added

---
+++ src/app/shared/hero/hero.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ServicesPreviewComponent } from "../services-preview/services-preview.component";
+import { CtaComponent } from "../cta/cta.component";
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {
+
+}
