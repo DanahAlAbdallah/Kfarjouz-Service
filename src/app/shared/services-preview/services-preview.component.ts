2c8d45f6c7e7c912eb8d3c648d37845c4b50ea9d
DanahAlAbdallah <danaalabdallah02@gmail.com>
2026-01-29 16:14:09 +0200

    all services are clickable now

--- src/app/shared/services-preview/services-preview.component.ts
+++ src/app/shared/services-preview/services-preview.component.ts
@@ -1,27 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-services-preview',
-  imports: [RouterModule],
-  templateUrl: './services-preview.component.html',
-  styleUrl: './services-preview.component.css',
+  standalone: true,
+  imports: [CommonModule,RouterModule], 
+  templateUrl: './services-preview.component.html'
 })
 export class ServicesPreviewComponent {
-    selectedCategory: string | null = null;
 
-  constructor(private route: ActivatedRoute,private router: Router) {}
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      this.selectedCategory = params['category'];
-    });
-  }
-
-
-
-  goToCategory(category: string) {
-    // Navigate to /services-list with query param ?category=food
-    this.router.navigate(['/services-list'], { queryParams: { category } });
-  }
+  services = [
+    {
+      title: 'Public professions and services',
+      icon: 'assets/icons/building.svg',
+      category: 'public'
+    },
+    {
+      title: 'Shops',
+      icon: 'assets/icons/shop.svg',
+      category: 'shop'
+    },
+    {
+      title: 'Cars and bikes',
+      icon: 'assets/icons/car'
+      ,
+      category: 'vehicle'
+    },
+    {
+      title: 'Home construction & maintenance',
+      icon: 'assets/icons/tools.svg',
+      category: 'home'
+    },
+    {
+      title: 'Food & Drink',
+      icon: 'assets/icons/food.svg',
+      category: 'food'
+    }
+  ];
 }
-
