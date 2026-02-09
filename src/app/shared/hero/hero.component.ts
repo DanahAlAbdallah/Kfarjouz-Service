4400103a3171cc3762bd27ccf76068f012c42577
DanahAlAbdallah <danaalabdallah02@gmail.com>
2026-02-09 23:33:46 +0200

    searching...

--- src/app/shared/hero/hero.component.ts
+++ src/app/shared/hero/hero.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { ServicesPreviewComponent } from "../services-preview/services-preview.component";
 import { CtaComponent } from "../cta/cta.component";
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [SearchBarComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
 export class HeroComponent {
 @Output() scrollToServices = new EventEmitter<void>();
-
+ searchQuery = signal('');
 }
